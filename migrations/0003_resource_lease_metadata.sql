a754d2b6e64e286d9b446bd4148f73568ef9b48d
Charlie Greenman <charlie@razroo.com>
2026-05-01 17:16:44 -0400

    chore: remove root sparsekernel migrations

--- migrations/0003_resource_lease_metadata.sql
+++
@@ -1,3 +0,0 @@
-ALTER TABLE resource_leases ADD COLUMN metadata_json TEXT;
-
-CREATE INDEX IF NOT EXISTS resource_leases_owner_status_updated_idx ON resource_leases(owner_agent_id, status, updated_at);
--- migrations/0004_resource_budgets.sql
+++
@@ -1,9 +0,0 @@
-INSERT OR IGNORE INTO runtime_info(key, value, updated_at)
-VALUES
-  ('resource_budget.logical_agents_max', '500', datetime('now')),
-  ('resource_budget.active_agent_steps_max', '100', datetime('now')),
-  ('resource_budget.model_calls_in_flight_max', '50', datetime('now')),
-  ('resource_budget.file_patch_jobs_max', '16', datetime('now')),
-  ('resource_budget.test_jobs_max', '4', datetime('now')),
-  ('resource_budget.browser_contexts_max', '2', datetime('now')),
-  ('resource_budget.heavy_sandboxes_max', '1', datetime('now'));
